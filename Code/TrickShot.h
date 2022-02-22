da1586918b9a9f9770ba7535d56dbba96bb68eba
Michał Nowak <bibbit@wp.pl>
2022-02-22 22:02:58 +0100

    Possibility to create area from given points

--- Code/TrickShot.h
+++ Code/TrickShot.h
@@ -7,7 +7,17 @@ namespace TrickShot
 	class TargetArea
 	{
 	public:
-		IntVector2 GetMin() const { return {}; }
-		IntVector2 GetMax() const { return {}; }
+		TargetArea() = default;
+		TargetArea(const IntVector2& minPoint, const IntVector2& maxPoint)
+			: m_min(minPoint)
+			, m_max(maxPoint)
+		{ }
+
+		const IntVector2& GetMin() const { return m_min; }
+		const IntVector2& GetMax() const { return m_max; }
+
+	private:
+		IntVector2 m_min{};
+		IntVector2 m_max{};
 	};
 }
--- Tests/Day17Tests.cpp
+++ Tests/Day17Tests.cpp
@@ -11,3 +11,13 @@ TEST(Day17, EmptyTargetArea)
 	EXPECT_EQ(emptyArea.GetMax().x, 0);
 	EXPECT_EQ(emptyArea.GetMax().y, 0);
 }
+
+TEST(Day17, NotEmptyTargetArea)
+{
+	TrickShot::TargetArea area{ { -100, 2 }, { -10, 20 } };
+
+	EXPECT_EQ(area.GetMin().x, -100);
+	EXPECT_EQ(area.GetMin().y, 2);
+	EXPECT_EQ(area.GetMax().x, -10);
+	EXPECT_EQ(area.GetMax().y, 20);
+}
