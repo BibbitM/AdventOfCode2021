45a7c93992f1c29f85848a3b4ea839c1720072fb
Michał Nowak <bibbit@wp.pl>
2022-04-18 22:07:48 +0200

    Create ScannerImage with given size

--- Code/ScannerImage.h
+++ Code/ScannerImage.h
@@ -3,6 +3,15 @@
 class ScannerImage
 {
 public:
-	int GetWidth() const { return 0; }
-	int GetHeight() const { return 0; }
+	ScannerImage() = default;
+	ScannerImage(int width, int height)
+		: m_width(width), m_height(height)
+	{ }
+
+	int GetWidth() const { return m_width; }
+	int GetHeight() const { return m_height; }
+
+private:
+	int m_width{};
+	int m_height{};
 };
--- Tests/Day20Tests.cpp
+++ Tests/Day20Tests.cpp
@@ -9,3 +9,11 @@ TEST(Day20, ScannerImageEmpty)
 	EXPECT_EQ(image.GetWidth(), 0);
 	EXPECT_EQ(image.GetHeight(), 0);
 }
+
+TEST(Day20, ScannerImageCreateGivenSize)
+{
+	ScannerImage image(10, 4);
+
+	EXPECT_EQ(image.GetWidth(), 10);
+	EXPECT_EQ(image.GetHeight(), 4);
+}
