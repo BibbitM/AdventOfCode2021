d57101839a631b1a487f382578dab24d55843ba8
Michał Nowak <bibbit@wp.pl>
2022-04-19 22:10:31 +0200

    Dirac::Dice can roll

--- Code/DiracDice.h
+++ Code/DiracDice.h
@@ -5,6 +5,12 @@ namespace Dirac
 	class Dice
 	{
 	public:
-		int NumRolls() const { return 0; }
+		int NumRolls() const { return m_numRolls; }
+
+		int Roll() { ++m_numRolls; return m_nextRoll++; }
+
+	private:
+		int m_numRolls{ 0 };
+		int m_nextRoll{ 1 };
 	};
 }
--- Tests/Day21Tests.cpp
+++ Tests/Day21Tests.cpp
@@ -8,3 +8,13 @@ TEST(Day21, DiracDiceEmpty)
 	
 	EXPECT_EQ(dice.NumRolls(), 0);
 }
+
+TEST(Day21, DiracDiceRollReturns)
+{
+	Dirac::Dice dice;
+
+	EXPECT_EQ(dice.Roll(), 1);
+	EXPECT_EQ(dice.Roll(), 2);
+	EXPECT_EQ(dice.Roll(), 3);
+	EXPECT_EQ(dice.NumRolls(), 3);
+}
