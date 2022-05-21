392c1d63ac7b0f3246a7047dc9cf6f551d2dad2b
Michał Nowak <bibbit@wp.pl>
2022-05-21 21:44:09 +0200

    Tested On/Off algorithm with example input

--- Tests/Day22Tests.cpp
+++ Tests/Day22Tests.cpp
@@ -5,6 +5,37 @@
 #include <sstream>
 #include <string>
 
+namespace
+{
+	constexpr const char c_exampleInput[] = R"--(on x=10..12,y=10..12,z=10..12
+on x=11..13,y=11..13,z=11..13
+off x=9..11,y=9..11,z=9..11
+on x=10..10,y=10..10,z=10..10)--";
+
+	constexpr const char c_exampleInput2[] = R"--(on x=-20..26,y=-36..17,z=-47..7
+on x=-20..33,y=-21..23,z=-26..28
+on x=-22..28,y=-29..23,z=-38..16
+on x=-46..7,y=-6..46,z=-50..-1
+on x=-49..1,y=-3..46,z=-24..28
+on x=2..47,y=-22..22,z=-23..27
+on x=-27..23,y=-28..26,z=-21..29
+on x=-39..5,y=-6..47,z=-3..44
+on x=-30..21,y=-8..43,z=-13..34
+on x=-22..26,y=-27..20,z=-29..19
+off x=-48..-32,y=26..41,z=-47..-37
+on x=-12..35,y=6..50,z=-50..-2
+off x=-48..-32,y=-32..-16,z=-15..-5
+on x=-18..26,y=-33..15,z=-7..46
+off x=-40..-22,y=-38..-28,z=23..41
+on x=-16..35,y=-41..10,z=-47..6
+off x=-32..-23,y=11..30,z=-14..3
+on x=-49..-5,y=-3..45,z=-29..18
+off x=18..30,y=-20..-8,z=-3..13
+on x=-41..9,y=-7..43,z=-33..15
+on x=-54112..-39298,y=-85059..-49293,z=-27449..7877
+on x=967..23432,y=45373..81175,z=27513..53682)--";
+}
+
 TEST(Day22, EmptyCube)
 {
 	const Cube cube{};
@@ -104,4 +135,51 @@ TEST(Day22, CubeMapOnOff)
 
 	map.Off(Cube({ 1, 1, 1 }, { 1, 1, 1 }));
 	EXPECT_EQ(map.Volume(), 2);
+}
+
+TEST(Day22, CubeMapFromExampleInput)
+{
+	CubeMap map;
+	std::istringstream in(c_exampleInput);
+
+	std::string op;
+	Cube cube;
+
+	while (in >> op >> cube)
+	{
+		if (op == "on")
+			map.On(cube);
+		else if (op == "off")
+			map.Off(cube);
+		else
+			ASSERT_TRUE(false);
+	}
+
+	EXPECT_EQ(map.Volume(), 39);
+}
+
+TEST(Day22, CubeMapFromExampleInput2)
+{
+	CubeMap map;
+	std::istringstream in(c_exampleInput2);
+
+	std::string op;
+	Cube cube;
+
+	Cube region({ -50, -50, -50 }, { 50, 50, 50 });
+
+	while (in >> op >> cube)
+	{
+		if (region.Intersection(cube) != cube)
+			continue;
+
+		if (op == "on")
+			map.On(cube);
+		else if (op == "off")
+			map.Off(cube);
+		else
+			ASSERT_TRUE(false);
+	}
+
+	EXPECT_EQ(map.Volume(), 590784);
 }
