e3ca2a7a671f6838188549755eddbc2e0b3e77bf
Michał Nowak <bibbit@wp.pl>
2022-05-21 14:30:18 +0200

    Creating cube from min & max points

--- Code/Cube.h
+++ Code/Cube.h
@@ -1,8 +1,21 @@
 #pragma once
 
+#include "IntVector3.h"
+
 class Cube
 {
 public:
+	Cube() = default;
+	Cube(const IntVector3& min, const IntVector3& max)
+		: min(min), max(max)
+	{}
 
-	int Volume() const { return 0; }
+	int Volume() const
+	{
+		return (max.x - min.x + 1) * (max.y - min.y + 1) * (max.z - min.z + 1);
+	}
+
+private:
+	IntVector3 min{};
+	IntVector3 max{};
 };
--- Tests/Day22Tests.cpp
+++ Tests/Day22Tests.cpp
@@ -6,5 +6,14 @@ TEST(Day22, EmptyCube)
 {
 	const Cube cube{};
 
-	EXPECT_EQ(cube.Volume(), 0);
+	EXPECT_EQ(cube.Volume(), 1);
+}
+
+TEST(Day22, CubeCtor)
+{
+	EXPECT_EQ(Cube({ 0, 0, 0 }, { 0, 0, 0 }).Volume(), 1);
+	EXPECT_EQ(Cube({ 0, 0, 0 }, { 1, 0, 0 }).Volume(), 2);
+	EXPECT_EQ(Cube({ 0, 0, 0 }, { 1, 1, 0 }).Volume(), 4);
+	EXPECT_EQ(Cube({ 0, 0, 0 }, { 1, 1, 1 }).Volume(), 8);
+	EXPECT_EQ(Cube({ -1, -1, -1 }, { 1, 1, 1 }).Volume(), 27);
 }
